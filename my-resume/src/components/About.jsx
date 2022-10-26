956b5c6dac4f1d58d49535f0c13deacaf696bfc0
Ryan Saucier <ryanyoubum@gmail.com>
2022-10-26 16:32:01 -0400

    more styling

--- my-resume/src/components/About.jsx
+++ my-resume/src/components/About.jsx
@@ -1,11 +1,15 @@
+import photoOfMe from './photos/me.jpeg'
+
 export default function About () {
     return(
 <div className="about">
-    <div className="photoOfMe">
-        <img src="./components/photos/me.jpeg"/>
-        <div className="section1">Section 1</div>
-    </div>
-
+    <div className="photoOfMe-container">
+        <img className="photoOfMe"src={photoOfMe}/>
+        </div>
+        <div className="section1">
+            <h1>Section 1</h1>
+            <p>About</p>
+        </div>
 </div>
     )
 }
